6f4fd51b240d774ec71cfcb65ee5ce95cad79a88
Van Quoc Thinh <thinhvan.231003@gmail.com>
2024-06-07 15:40:18 +0700

    SETUP

---
+++ jewelry_store/src/test/java/com/jewelry_store/jewelry_store/JewelryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewelry_store.jewelry_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
