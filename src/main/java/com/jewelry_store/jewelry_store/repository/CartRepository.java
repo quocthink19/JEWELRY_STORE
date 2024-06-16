f07433f921e7026b37a155dd7a7dc92a659d6d41
Tri Coder <Anhtri22303@gmail.com>
2024-06-16 15:09:14 +0700

    .

--- src/main/java/com/jewelry_store/jewelry_store/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jewelry_store.jewelry_store.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jewelry_store.jewelry_store.model.Cart;
-
-public interface CartRepository extends JpaRepository<Cart,Long> {
-
-}
--- src/main/java/com/jewelry_store/jewelry_store/response/MessageResponse.java
+++
@@ -1,9 +0,0 @@
-package com.jewelry_store.jewelry_store.response;
-
-import lombok.Data;
-
-@Data
-public class MessageResponse {
-
-    private String message;
-}
